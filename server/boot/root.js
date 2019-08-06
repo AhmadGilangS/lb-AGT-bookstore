0c1566c8d12822dfaec2b12b15901ea6794a5eca
Gina Rizka <ginarizkaa@gmail.com>
2019-08-06 15:59:54 +0700

    api product by gina

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,9 +1,7 @@
-// Copyright IBM Corp. 2016. All Rights Reserved.
-// Node module: loopback-workspace
+// Copyright IBM Corp. 2014,2016. All Rights Reserved.
+// Node module: loopback-example-database
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
-
-'use strict';
 
 module.exports = function(server) {
   // Install a `/` route that returns server status
